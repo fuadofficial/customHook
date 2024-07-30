56b0558a45f55bac5eedf8ed68d3be64e794004d
fuad <ahmadfuadoorakam@gmail.com>
2024-07-30 22:47:12 +0530

    some

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Button.css'
+
+const Button = () => {
+    return (
+        <div className='container'>
+            <input type="text" />
+            <button>Show/Hide</button>
+        </div>
+    )
+}
+
+export default Button
